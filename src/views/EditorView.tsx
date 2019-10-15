b562cba552eaa889a8d32a529755837bab0686e5
shams mosowi <shams.mosowi@gmail.com>
2019-10-15 17:46:39 +1100

    custom block

--- src/views/EditorView.tsx
+++ src/views/EditorView.tsx
@@ -1,9 +1,29 @@
 import React, { Component } from "react";
-import { GEditor } from "grapesjs-react";
+import { GEditor, GBlock, GComponent } from "grapesjs-react";
 import "grapesjs/dist/css/grapes.min.css";
+
+class SimpleBlock extends GBlock {
+  content = `<div class="simple-block"> This is a simple block </div>`;
+
+  constructor() {
+    super("simple-block", "Simple Block");
+  }
+}
+
 class GEditorExample extends Component {
   render() {
-    return <GEditor id="editor" webpage={true} />;
+    return (
+      <GEditor
+        id="editor"
+        blocks={[new SimpleBlock()]}
+        plugins={[
+          (props: any) => {
+            console.log(props);
+          },
+        ]}
+        webpage={true}
+      />
+    );
   }
 }
 
